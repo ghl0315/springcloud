95474ba891cb7ba501b48ec0331279df8bda6a56
guhuanglong <ghl0315@163.com>
2021-02-19 10:34:17 +0800

    注册新服务

---
+++ product-data-service/src/main/java/cn/how2j/springcloud/ProductDataServiceApplication.java
@@ -0,0 +1,31 @@
+package cn.how2j.springcloud;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.NetUtil;
+import cn.hutool.core.util.NumberUtil;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProductDataServiceApplication {
+    public static void main(String[] args) {
+        int port = 0;
+        int defaultPort = 8001;
+
+        if(!NetUtil.isUsableLocalPort(port)) {
+            System.err.printf("端口%d被占用了，无法启动%n", port );
+            System.exit(1);
+        }
+
+        new SpringApplicationBuilder(ProductDataServiceApplication.class).properties("server.port=" + 8001).run(args);
+    }
+}
+
